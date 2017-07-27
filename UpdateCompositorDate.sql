8f6867c52329a7e5640c4c86a927f97526740c23
elekis <elekis@cmaj9.com>
2017-07-27 14:12:56 +0200

    update script with wikipedia search

---
+++ UpdateCompositorDate.sql
@@ -0,0 +1,21 @@
+select * from compositors where birth = ""
+
+UPDATE compositors  SET birth = 1860, death = 1909 where name  ="Isaac Albeniz";
+UPDATE compositors  SET birth = 1913, death = 1976 where name  ="Benjamin Britten";
+UPDATE compositors  SET birth = 1856, death = 1870 where name  ="Dmitri Balakirev";
+UPDATE compositors  SET birth = 1848, death = 1933 where name  ="Henri Duparc";
+UPDATE compositors  SET birth = 1770, death = 1827 where name  ="Ludwig van Beethoven";
+UPDATE compositors  SET birth = 1927 where name  ="Seymour Bernstein";
+UPDATE compositors  SET birth = 1852, death = 1918 where name  ="Claude Debussy";
+UPDATE compositors  SET birth = 1845, death = 1924  where name  ="Gabriel Fauré";
+UPDATE compositors  SET birth = 1883, death = 1947 where name  ="Alfredo Casella";
+UPDATE compositors  SET birth = 1810, death =  1856 where name  ="Robert Schumann";
+
+UPDATE compositors  SET birth = 1917, death = 1995 where name  ="Isang Yun";
+UPDATE compositors  SET birth = 1960, death =  "" where name  ="Nigel Clarke";
+UPDATE compositors  SET birth = 1846, death = 1916 where name  ="Francesco Tosti";
+UPDATE compositors  SET birth = 1900, death = 1976 where name  ="Michael Head";
+UPDATE compositors  SET birth = 1934, death = ""  where name  ="Pavle Despalj";
+UPDATE compositors  SET birth = 1809, death = 1847 where name  ="Felix Mendelssohn";
+UPDATE compositors  SET birth = 1812, death = 1865 where name  ="Heinrich Wilhelm Ernst";
+
--- getInfo.py
+++ getInfo.py
@@ -8,13 +8,14 @@ import sqlite3
 import datetime
 import hashlib
 import os
-
+import wikipedia
+import csv
 try: 
     from BeautifulSoup import BeautifulSoup
 except ImportError:
     from bs4 import BeautifulSoup
 
-CompositorManager = []
+composerManager = []
 OeuvreManager = []
 ActivitiesManager = []
 universal_ID = 8765
@@ -25,19 +26,31 @@ def get_a_id():
     universal_ID += 1;
     return universal_ID;
 
-def get_data(url):
+def get_composer(url):
     if not os.path.exists(directory_raw_info):
         os.makedirs(directory_raw_info)
+    if not os.path.exists(directory_raw_info+"/c"):
+        os.makedirs(directory_raw_info+"/c")
+    return get_data(url,"c/")
+
+def get_oeuvre(url):
+    if not os.path.exists(directory_raw_info):
+        os.makedirs(directory_raw_info)
+    if not os.path.exists(directory_raw_info+"/o"):
+        os.makedirs(directory_raw_info+"/o")
+    return get_data(url,"o/")
+    
+    
+def get_data(url,o_dir):    
     file_name = hashlib.sha224(url.encode('utf-8')).hexdigest()
-    if os.path.isfile(directory_raw_info+"/"+file_name):
-        F = open(directory_raw_info+"/"+file_name,"r")
+    if os.path.isfile(directory_raw_info+"/"+o_dir+file_name):
+        F = open(directory_raw_info+"/"+o_dir+file_name,"r")
         return F.read()
     else:
-        print("download")
         opener = urllib.request.FancyURLopener({})       
         f = opener.open(url)
         content = f.read()
-        F = open(directory_raw_info+"/"+file_name,"wb")
+        F = open(directory_raw_info+"/"+o_dir+file_name,"wb")
         F.write(content)
         return content
 
@@ -45,12 +58,16 @@ def get_data(url):
 def replace_letter(strg):
     global issue
     a = strg.strip()
-    if a == "PremiÃ¨re Ã©preuve" : a = "Première épreuve"
-    if a == "Michael Ponti"      : a = "Michaël Ponti"
-    if a == "Lukas Vondracek"    : a = "Lukáš Vondráček"
-    if a == "Pierre Volondat"    : a = "Pierre-Alain Volondat"
-    if a == "Mikhail Faerman"    : a = "Mikhaïl Faerman"
+    if a == "PremiÃ¨re Ã©preuve"   : a = "Première épreuve"
+    if a == "Michael Ponti"        : a = "Michaël Ponti"
+    if a == "Lukas Vondracek"      : a = "Lukáš Vondráček"
+    if a == "Pierre Volondat"      : a = "Pierre-Alain Volondat"
+    if a == "Mikhail Faerman"      : a = "Mikhaïl Faerman"
     if a == "Eketarina Novitzkaya" : a = "Ekaterina Novitskaya"
+    if a == "Concert de lauréats"  : a = "Concert de lauréats"
+    if a == "Récital du prix Musiq'3"  : a = "Récital du prix Musiq3"
+    if a == "Zbigniew Tursky"      : a = "Zbigniew Turski"
+    if a == "Emil Guilels"          : a = "Emil Gilels"
     
     a = a.replace("Ã©","é").replace("Ã¨","è").replace('”','').replace('“','').replace('Ã¿','ÿ').replace("Ã§","ç").replace("Ã¢","â").replace("Ã«","ë").replace('Ã¶','ö').replace('Ã³','ó').replace('Ã±','ñ').replace('Ã¯','ï').replace(' - récital','')
     if re.search('[^0-9a-zA-Zû\ \.éèêë\-aáà\:uù\,óůü\/\(\)\?ôçö&\!üïÜÖîœääìÿòřáěčÈòÁıöŞł…ÔřáířáÉ\'ñﬂý°\[\]¿\%ßFånämöŒøâme∆ÈÏ’ÄúlÀ\–Ž«»`ňáąCançãoGarõaŠirokérukáširt’s ‘igaroyžmná’!Dzieciątkomójmaleńki]+', a) is not None:
@@ -72,21 +89,21 @@ class Activities :
                 "+++self.annee"      + str(self.annee)    +"\n"+
                 "+++self.classement" +str(self.classement)+"\n")
 
-class Compositor : 
+class Composer : 
     
-    def __init__(self,name,compositor_link,f):
+    def __init__(self,name,composer_link,f):
         self.id = get_a_id()
-        self.url = compositor_link
+        self.url = composer_link
         self.name = replace_letter(name)
         self.oeuvres = [f]
         self.birth = ""
-        self.death = ""        
-        self.content = get_data(self.url)
+        self.death = ""   
+        self.wikipedia_url = ""
+        self.content = get_composer(self.url)
         try:
             self.content= self.content.decode("utf-8")
         except AttributeError:
-            pass
-            #
+            pass    
         m = re.search('°(?P<b>\d+) \- †(?P<d>\d+)', self.content)   
         try :       
             self.birth = m.group("b")
@@ -97,17 +114,25 @@ class Compositor :
                 self.birth = m.group("b")
                 self.death = ""            
 #               print(self.birth, self.death, ':',self.name,self.url)
-                self.oeuvres = [f]
             except AttributeError:
-                self.birth = ""
-                self.death = ""            
-                self.oeuvres = [f]
-                
-#    def getCRECompositor(self):
-#        opener = urllib.request.FancyURLopener({})       
-#        f = opener.open(self.url)
-#        content = f.read()
-#        return content
+                try : #Isaac Manuel Francisco Albéniz y Pascual (Spanish pronunciation: [iˈsaːk alˈβeniθ]; 29 May 1860 – 18 May 1909)
+                    d = wikipedia.summary(self.name+ " classical", 2)
+                    m = re.search('\d\d? .* (?P<birth>\d+) – \d\d? .* (?P<death>\d+)\)', d)
+                    self.birth = m.group("birth")
+                    self.death = m.group("death")    
+                except Exception:
+                    try :
+                        m = re.findall('1\d\d\d', d)
+                        print(self.name,m, d)
+                        if(len(m) == 0) :     
+                            print("NOT FOUND")
+                        else :
+                            self.birth = m[0]
+                            if(len(m) > 1) : self.death = m[1]               
+                    except Exception as e:                         
+                        print("exeption line 131  ",self.name,"  ", e)
+        self.oeuvres = [f]
+
     
     def __str__(self):
         return str( "++name :"     + str(self.name)+"\n" +
@@ -125,26 +150,25 @@ class CREOeuvre :
         self.id = get_a_id();
         self.page_number = page_number
         self.url_oeuvre  = "http://cmireb.be/cgi?lg=fr&pag=1942&tab=227&rec="+str(page_number)+"&frm=0&par=secorig1656"
-        
-        print(self.url_oeuvre)
-        self.content = get_data(self.url_oeuvre)
+        #print(self.url_oeuvre)
+        self.content = get_oeuvre(self.url_oeuvre)
        # print(self.content)
         self.parsed_html = BeautifulSoup(self.content,"html.parser")        
         self.oeuvre_name = replace_letter(self.getOeuvreName())
-        self.compositor_name = replace_letter(self.getCompositor())
+        self.composer_name = replace_letter(self.getcomposer())
         self.instruments = self.getInstruments()
-        self.compositor_link = self.getCompositorLink()
+        self.composer_link = self.getcomposerLink()
         self.activities = self.getActivities()  
       
-        if self.compositor_link != None :
-            f =  [elem for elem in CompositorManager if elem.name == self.compositor_name]
+        if self.composer_link != None :
+            f =  [elem for elem in composerManager if elem.name == self.composer_name]
             assert(len(f) <= 1)
 
             if len(f) == 0 :
-                self.compositor_object = Compositor(self.compositor_name,self.compositor_link,self)
-                CompositorManager.append(self.compositor_object);
+                self.composer_object = Composer(self.composer_name,self.composer_link,self)
+                composerManager.append(self.composer_object);
             else :
-                self.compositor_object = f[0]
+                self.composer_object = f[0]
                 f[0].addOeuvre(self);
             
         
@@ -152,12 +176,12 @@ class CREOeuvre :
     def __str__(self):
         return str( "page_number :"     + str(self.page_number)+"\n" +
                     "oeuvre_name :"     + str(self.oeuvre_name)+"\n" +
-                    "compositor  :"     + str(self.compositor_name)+"\n" +
+                    "composer  :"     + str(self.composer_name)+"\n" +
                     "instruments :"     + str(self.instruments)+"\n" +
                     "oeuvre_url  :"     + str(self.url_oeuvre)+"\n" +
-                    "compositor_link :" + str(self.compositor_link)+"\n" +
+                    "composer_link :" + str(self.composer_link)+"\n" +
                     "activities :"      + str(self.activities)+"\n" +
-                    "compositor : \n"    + str(self.compositor_object)+"\n" )
+                    "composer : \n"    + str(self.composer_object)+"\n" )
 #http://cmireb.be/cgi?lg=fr&pag=1698&tab=102&rec=1013&frm=0
 #http://cmireb.be/cgi?lg=fr&pag=1942&tab=227&rec=417&frm=0&par=secorig1656
  #   def getCREOeuvre(self):
@@ -172,7 +196,7 @@ class CREOeuvre :
     def getOeuvreName(self):
         return self.getOeuvreTitle().split(':',1)[1].strip().replace('"',"").replace("'","")
     
-    def getCompositor(self):
+    def getcomposer(self):
         return self.getOeuvreTitle().split(':',1)[0].strip()
      
         
@@ -190,12 +214,27 @@ class CREOeuvre :
             return row_data
         return row_data
     
-    def getCompositorLink(self):
-            table = self.parsed_html.body.find('table',{"class":"listeFonctions"})
-            if table == None : return None
-            a = str(table.find("a")).split('"')[1].replace ("&amp;","&")       
-            assert(a != None)
-            return "http://cmireb.be"+a
+    def getcomposerLink(self):
+        table = self.parsed_html.body.find('table',{"class":"listeFonctions"})
+        if table == None : return None
+
+        #<a href="/cgi?lg=fr&amp;pag=1698&amp;tab=102&amp;rec=1316&amp;frm=0">Maurice Ravel</a></td> <td class="col2">Compositeur</td>
+        #a = str(table.find("a")).split('"')[1].replace ("&amp;","&")       
+        for tr in table.find_all('tr')[2:]:
+            tds = tr.find_all('td')
+            for i in range(0,len(tds)) :
+                if tds[i].text == "Compositeur" :
+                    url = tds[0].a.get('href')
+                    return "http://cmireb.be"+url
+        for tr in table.find_all('tr')[2:]:
+            tds = tr.find_all('td')
+            for i in range(0,len(tds)) :
+                if tds[i].text == "Auteur" :
+                    url = tds[0].a.get('href')
+                    return "http://cmireb.be"+url
+                
+        a = str(table.find("a")).split('"')[1].replace ("&amp;","&") 
+        return "http://cmireb.be/"+a
             
     #listeActivites
     def getActivities(self):  
@@ -231,45 +270,89 @@ if __name__ == "__main__":
     conn = sqlite3.connect('CRE.db')
     c = conn.cursor()
 
+    
+    c.execute("CREATE table winners (annee ,first,second,third,fourth,fifth,categorie)") # use your column names here
+
+    with open('Winners.csv') as fin: # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.reader(fin) # comma is default delimiter
+        for r in dr : 
+            e = 'INSERT INTO winners VALUES ("'+'","'.join([x.strip() for x in r])+'")'
+            c.execute(e)
+
+            
+    c.execute("CREATE table categories (annee ,name)") # use your column names here
+    with open('Categories.csv') as fin: # `with` statement available in 2.5+
+        # csv.DictReader uses first line in file for column headings by default
+        dr = csv.reader(fin) # comma is default delimiter
+        for r in dr : 
+            e = 'INSERT INTO categories VALUES ("'+'","'.join([x.strip() for x in r])+'")'
+            c.execute(e)
+
+            
     # Create table
-    c.execute('''CREATE TABLE IF NOT EXISTS oeuvres(id,page_number,oeuvre_name,compositor_name, compositor_id,oeuvre_link)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS oeuvres(id,page_number,oeuvre_name,composer_name, composer_id,oeuvre_link)''')
     c.execute('''CREATE TABLE IF NOT EXISTS instruments(id,oeuvre_id,instrument,role,type)''')
     c.execute('''CREATE TABLE IF NOT EXISTS activities(id,oeuvre_id,annee,nom_musicien,classement)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS compositors(id,name,birth,death,url)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS linktableOC(compositor_id,oeuvres_id)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS composers(id,name,birth,death,url,wikipediaUrl)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS linktableOC(composer_id,oeuvres_id)''')
     
-    count = 0
     for i in range (1,9000):
             o = CREOeuvre(i);
-            if o.oeuvre_name.strip() != "" and o.compositor_link != None:
+            if o.oeuvre_name.strip() != "" and o.composer_link != None:
                 OeuvreManager.append(o)
-            else :
-                count += 1
-    print (count,"/",9000)
             
     for e in OeuvreManager:
         if e == None : 
             print("oeuvre None")
             continue
-        c.execute('INSERT INTO oeuvres VALUES ("' + '","'.join([str(e.id),str(e.page_number),e.oeuvre_name,e.compositor_name,str(e.compositor_object.id),str(e.url_oeuvre)])+ '")')
+        c.execute('INSERT INTO oeuvres VALUES ("' + '","'.join([str(e.id),str(e.page_number),e.oeuvre_name,e.composer_name,str(e.composer_object.id),str(e.url_oeuvre)])+ '")')
         
-        c.execute("INSERT INTO linktableOC Values ("'"'  + '","'.join([str(e.compositor_object.id),str(e.id)])   +'")')
+        c.execute("INSERT INTO linktableOC Values ("'"'  + '","'.join([str(e.composer_object.id),str(e.id)])   +'")')
         
         for i in e.instruments:
             if i == None : continue
             c.execute('INSERT INTO instruments Values("' + '","'.join([str(get_a_id()),str(e.id),i[0],i[1],i[2]])+ '")')
         
     
-    for e  in CompositorManager:
+    for e  in composerManager:
         if e == None : continue
-        c.execute( 'INSERT INTO compositors VALUES ("' + '","'.join([str(e.id),e.name,str(e.birth),str(e.death),e.url])+'")')
+        c.execute( 'INSERT INTO composers VALUES ("' + '","'.join([str(e.id),e.name,str(e.birth),str(e.death),e.url,e.wikipedia_url])+'")')
 
     for e  in ActivitiesManager:
         if e == None : continue
         c.execute( 'INSERT INTO activities VALUES ("' + '","'.join([str(e.id),str(e.oeuvre.id),str(e.annee),str(e.nom_musicien),e.classement])+'")')
 
-    conn.commit()
+    c.execute ('''create view global_stat as select "nombre de compositeurs",count(*) from composers
+union
+select "nombre d'oeuvres",count(*) from oeuvres
+union
+select "nombre de compositeurs (birth != null)",count(*) from composers where birth != ""
+union
+select "nombre de compositeurs Piano ",count(*) from composers where  id 
+in (select composer_id from instruments, oeuvres, composers where instruments.oeuvre_id = oeuvres.id and composers.id = oeuvres.composer_id  and instrument = "piano")
+union
+select "nombre de compositeurs Piano (birth != null)",count(*) from composers where birth != "" and id 
+in (select composer_id from instruments, oeuvres, composers where instruments.oeuvre_id = oeuvres.id and composers.id = oeuvres.composer_id and birth != ""  and instrument = "piano")
+union
+select "nombres de sonates (Piano)", count(*) from oeuvres where oeuvres.id in 
+(select oeuvre_id from instruments, oeuvres, composers where instruments.oeuvre_id = oeuvres.id and composers.id = oeuvres.composer_id and birth != ""  and instrument = "piano" and oeuvre_name like "%sonate%")
+union
+select "nombres de concerto (Piano)", count(*) from oeuvres where oeuvres.id in 
+(select oeuvre_id from instruments, oeuvres, composers where instruments.oeuvre_id = oeuvres.id and composers.id = oeuvres.composer_id and birth != ""  and instrument = "piano" and oeuvre_name like "%concerto%")''')
+    
+    c.execute('''create view W2 as select * from activities where nom_musicien in( select first from winners) and classement = "Finale"''')
+    
+    c.execute('''create view winnerOeuvre  as select * from activities,oeuvres where nom_musicien in( select first from winners) and  oeuvre_id = oeuvres.id and annee in (select annee from categories where  name = "Piano") and classement = "Finale" order by  cast (annee as INT)
+''')
+    
+    c.execute('''create view  stat_composer_winner as select composer_name, count(*) as c from winnerOeuvre group by composer_name order by c desc ''')
+    c.execute('''create view  stat_composer_Final as select composer_name,count (*)  as c from activities, oeuvres where oeuvres.id = activities.oeuvre_id  and classement in ("Finale") group by composer_name order by c desc''')
+    c.execute('''create view  stat_composer_Demi_Final as select composer_name,count (*)  as c from activities, oeuvres where oeuvres.id = activities.oeuvre_id  and classement in ("Demi-finale") group by composer_name order by c desc''')
+    c.execute('''create view  stat_composer_Premiere_ep as select composer_name,count (*)  as c from activities, oeuvres where oeuvres.id = activities.oeuvre_id  and classement in ("Première épreuve") group by composer_name order by c desc''')
 
+    c.execute('''create view winnerConcerto as   select * from activities,oeuvres where nom_musicien in( select first from winners) and  oeuvre_id = oeuvres.id and annee in (select annee from categories where  name = "Piano") and classement = "Finale" and oeuvre_name like "%Concerto%" ''')
+    conn.commit()
     conn.close()
     print(issue)
     print(str(datetime.datetime.today()))
